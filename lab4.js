47823c262ebfdd760ecffbbc01f41a637cef8dc6
Jon Sorrells <jlsorrells@wpi.edu>
2015-02-13 15:01:16 -0500

    draws a tree map

--- lab4.js
+++ lab4.js
@@ -5,45 +5,29 @@ var largeRect = true;
 
 //Make an SVG Container
 var svgContainer = d3.select("body").append("svg")
-                                    .attr("width", 400)
-                                    .attr("height", 300);
+                                    .attr("width", 800)
+                                    .attr("height", 500);
 
-var rect = svgContainer.append("rect").attr("x", 100)
-                                      .attr("y", 75)
-                                      .attr("width", 200)
-                                      .attr("height", 150)
-                                      .attr("fill", "blue")
-                                      .on("click", changeRect);
-                           
-var text = svgContainer.append("text").attr("x", 200)
-                                      .attr("y", 150)
-                                      .attr("font-size", 30)
-                                      .attr("text-anchor", "middle")
-                                      .attr("alignment-baseline", "central")
-                                      .text("text 1");
-function changeRect() {
-    if (largeRect) {
-        // make it small
-        rect.transition().duration(500)
-            .attr("width", 400 / 3)
-            .attr("height", 100)
-            .attr("x", 400 / 3)
-            .attr("y", 100)
-            .attr("fill", "green");
-        
-        text.text("text 2");
-    } else {
-        // make it large
-        rect.transition().duration(500)
-            .attr("width", 200)
-            .attr("height", 150)
-            .attr("x", 100)
-            .attr("y", 75)
-            .attr("fill", "blue");
-        
-        text.text("text 1");
-    }
-    largeRect = !largeRect;
+var treemap = d3.layout.treemap().size([800, 500])
+    .value(function (d) { return d.size; });
+    
+var color = d3.scale.category20();
+
+d3.json("test.json", doStuff);
+
+function doStuff(error, root) {
+    var node = svgContainer.datum(root).selectAll(".node")
+        .data(treemap.nodes)
+        .enter().append("rect")
+            .attr("fill", function (d) { return color(d.name); })
+            .attr("stroke-width", function (d) { return 10 - 2 * d.depth; })
+            .attr("stroke", "black")
+            .call(position);
 }
 
-
+function position() {
+  this.attr("x", function(d) { return d.x + "px"; })
+      .attr("y", function(d) { return d.y + "px"; })
+      .attr("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+      .attr("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+}
