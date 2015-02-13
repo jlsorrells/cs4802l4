b1d719b1eb103ae1a6859f531bfc45ed69e2a36a
Jon Sorrells <jlsorrells@wpi.edu>
2015-02-13 13:08:49 -0500

    initail commit for lab 4

---
+++ README.md
@@ -0,0 +1,3 @@
+Jon Sorrells, lab 4, cs 4802
+To view the output, go to http://jlsorrells.github.io/cs4802l4/  
+ 
---
+++ lab4.js
@@ -0,0 +1,12 @@
+
+"use strict"
+
+//Make an SVG Container
+var svgContainer = d3.select("body").append("svg")
+                                    .attr("width", 1000)
+                                    .attr("height", 600);
+
+
+
+
+
