2842982b3a9691a12ba666eb48ab4e6d6bc3f200
Jon Sorrells <jlsorrells@wpi.edu>
2015-02-15 14:50:14 -0500

    use data from assignment 2

--- lab4.js
+++ lab4.js
@@ -1,7 +1,7 @@
 
 "use strict"
 
-var largeRect = true;
+var depthColor = 2;
 
 //Make an SVG Container
 var svgContainer = d3.select("body").append("svg")
@@ -9,7 +9,7 @@ var svgContainer = d3.select("body").append("svg")
                                     .attr("height", 600);
 
 var treemap = d3.layout.treemap().size([800, 600])
-    .value(function (d) { return d.size; });
+    .value(function (d) { return (d.name.match(/\n/g) || []).length + 1; });
     
 var color = d3.scale.category20b();
 
@@ -21,11 +21,12 @@ function doStuff(error, root) {
         .enter().append("g");
     node.append("rect")
         .attr("fill", function (d) { return color(d.name); })
+        .attr("opacity", function (d) { return d.depth == depthColor ? 1 : .05 })
         .attr("stroke-width", 2)
         .attr("stroke", "black")
-        .attr("opacity", function (d) { return Math.pow((1 - d.depth / 5), 2); })
-        .call(position);
-    node.append("text").text(function(d) { return d.children ? null : d.name; })
+        .call(position)
+        .on("click", function () {depthColor = d3.select(this).transition().duration(500).attr("fill", "blue");});
+    node.append("text").text(function(d) { return d.children ? null : d.name.replace(/\n/g,", "); })
         .attr("x", function(d) { return d.x + "px"; })
         .attr("y", function(d) { return d.y + 10 + "px"; })
         .attr("font-size", 10 + "px");
